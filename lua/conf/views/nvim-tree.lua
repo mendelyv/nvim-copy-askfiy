60c085914fb88587f4f1a2a51cc0007239ff0e06
mendelyv <1064137887@qq.com>
2023-02-20 12:30:50 +0800

    调整nvim-tree配置，文件转换焦点时树同时高亮

--- lua/conf/views/nvim-tree.lua
+++ lua/conf/views/nvim-tree.lua
@@ -16,40 +16,139 @@ end
 
 function M.load()
     M.nvim_tree.setup({
-        disable_netrw = false,
-        hijack_netrw = false,
-        hijack_cursor = true,
-        update_cwd = true,
-        ignore_ft_on_setup = { "dashboard" },
-        reload_on_bufenter = true,
-        notify = {
-            threshold = vim.log.levels.WARN,
-        },
-        update_focused_file = {
-            enable = true,
-            update_cwd = false,
-        },
+        -- disable_netrw = false,
+        -- hijack_netrw = false,
+        -- hijack_cursor = true,
+        -- update_cwd = true,
+        -- ignore_ft_on_setup = { "dashboard" },
+        -- reload_on_bufenter = true,
+        -- notify = {
+        --     threshold = vim.log.levels.WARN,
+        -- },
+        -- update_focused_file = {
+        --     enable = true,
+        --     update_cwd = false,
+        -- },
+        -- view = {
+        --     number = true,
+        --     relativenumber = true,
+        --     side = "left",
+        --     width = 30,
+        --     hide_root_folder = false,
+        --     signcolumn = "yes",
+        --     mappings = {
+        --         list = {
+        --             { key = "?", action = "toggle_help" },
+        --         },
+        --     },
+        -- },
+        -- diagnostics = {
+        --     enable = true,
+        --     show_on_dirs = true,
+        --     icons = {
+        --         hint = icons.Hint,
+        --         info = icons.Info,
+        --         warning = icons.Warn,
+        --         error = icons.Error,
+        --     },
+        -- },
+        -- actions = {
+        --     use_system_clipboard = true,
+        --     change_dir = {
+        --         enable = true,
+        --         global = true,
+        --         restrict_above_cwd = false,
+        --     },
+        --     open_file = {
+        --         resize_window = true,
+        --         window_picker = {
+        --             enable = true,
+        --         },
+        --     },
+        -- },
+        -- trash = {
+        --     cmd = "trash",
+        --     require_confirm = true,
+        -- },
+        -- filters = {
+        --     dotfiles = false,
+        --     custom = { "node_modules", "\\.cache", "__pycache__" },
+        --     exclude = {},
+        -- },
+        -- renderer = {
+        --     add_trailing = true,
+        --     highlight_git = true,
+        --     highlight_opened_files = "none",
+        --     icons = {
+        --         show = {
+        --             file = true,
+        --             folder = true,
+        --             folder_arrow = true,
+        --             git = false,
+        --         },
+        --         glyphs = {
+        --             default = "",
+        --             symlink = "",
+        --             git = {
+        --                 unstaged = "",
+        --                 staged = "",
+        --                 unmerged = "",
+        --                 renamed = "凜",
+        --                 untracked = "",
+        --                 deleted = "",
+        --                 ignored = "",
+        --             },
+        --             folder = {
+        --                 arrow_open = "",
+        --                 arrow_closed = "",
+        --                 default = "",
+        --                 open = "",
+        --                 empty = "",
+        --                 empty_open = "",
+        --                 symlink = "",
+        --                 symlink_open = "",
+        --             },
+        --         },
+        --     },
+        -- },
+        -- 自动关闭
+        -- auto_close = true,
+        -- 视图
         view = {
+            -- 宽度
+            width = 35,
+            -- 高度
+            -- height = 30,
+            -- 隐藏顶部的根目录显示
+            hide_root_folder = false,
+            -- 自动调整大小
+            -- auto_resize = true,
+            side = "left",
             number = true,
             relativenumber = true,
-            side = "left",
-            width = 30,
-            hide_root_folder = false,
-            signcolumn = "yes",
-            mappings = {
-                list = {
-                    { key = "?", action = "toggle_help" },
-                },
-            },
         },
         diagnostics = {
+            -- 是否启用文件诊断信息
             enable = true,
-            show_on_dirs = true,
             icons = {
-                hint = icons.Hint,
-                info = icons.Info,
-                warning = icons.Warn,
-                error = icons.Error,
+                hint = " ",
+                info = " ",
+                warning = " ",
+                error = "",
+            },
+        },
+        git = {
+            -- 是否启用 git 信息
+            enable = false,
+            ignore = false,
+            timeout = 500,
+        },
+        filters = {
+            -- 文件过滤器
+            dotfiles = false,
+            custom = {
+                "*.meta",
+                "*.DS_Store",
             },
         },
         actions = {
@@ -66,50 +165,9 @@ function M.load()
                 },
             },
         },
-        trash = {
-            cmd = "trash",
-            require_confirm = true,
-        },
-        filters = {
-            dotfiles = false,
-            custom = { "node_modules", "\\.cache", "__pycache__" },
-            exclude = {},
-        },
-        renderer = {
-            add_trailing = true,
-            highlight_git = true,
-            highlight_opened_files = "none",
-            icons = {
-                show = {
-                    file = true,
-                    folder = true,
-                    folder_arrow = true,
-                    git = false,
-                },
-                glyphs = {
-                    default = "",
-                    symlink = "",
-                    git = {
-                        unstaged = "",
-                        staged = "",
-                        unmerged = "",
-                        renamed = "凜",
-                        untracked = "",
-                        deleted = "",
-                        ignored = "",
-                    },
-                    folder = {
-                        arrow_open = "",
-                        arrow_closed = "",
-                        default = "",
-                        open = "",
-                        empty = "",
-                        empty_open = "",
-                        symlink = "",
-                        symlink_open = "",
-                    },
-                },
-            },
+        update_focused_file = {
+            enable = true,
+            update_cwd = false,
         },
     })
 end
